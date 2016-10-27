4f351ed71119ee9e429cb9bd5402454298af3a59
Brecht Devleesschauwer <brechtdv@gmail.com>
2016-10-27 09:49:55 +0200

    introduce publications

--- vitae-md.R
+++ vitae-md.R
@@ -99,6 +99,86 @@ print_article <-
     cat("\n\n")
   }
 
+
+
+print_article2 <-
+  function(item, id) {
+    ## reformat authors string
+    authors_string <- strsplit(item$authors, ";")[[1]]
+    authors_string <- sapply(authors_string, trim, USE.NAMES = FALSE)
+    authors_string <- sapply(authors_string, abbr, USE.NAMES = FALSE)
+    
+    ## reformat year string
+    year_string <- item$year
+    if (!is.na(item$year)) {
+      year_string <- paste0(" (", item$year, ") ")
+    } else {
+      year_string <- "."
+    }
+    
+    ## reformat title string
+    title_string <- item$title
+    
+    for (i in seq_along(needle))
+      title_string <- gtag(title_string, needle[i], "_")
+    
+    if (substring(title_string, nchar(title_string)) != "?")
+      title_string <- paste0(title_string, ".")
+    
+    ## reformat journal string
+    journal_string <- gsub(".", "", item$journal_short, fixed = TRUE)
+    journal_string <- tag(journal_string, "_")
+    
+    ## reformat volume/page string
+    if (is.na(item$year)) {
+      vp_string <- ""
+      
+    } else if (is.na(item$volume)) {
+      vp_string <- ", in press"
+      
+    } else if (!is.na(item$eID)) {
+      vp_string <- paste0(" ", item$volume, ":", item$eID)
+      
+    } else {
+      vp_string <- paste0(" ", item$volume, ":", item$from, "-", item$to)
+    }
+    
+    ## reformat DOI string
+    doi_string <- item$DOI
+    if (!is.null(doi_string) && !is.na(doi_string)) {
+      doi_string <-
+        sprintf(". doi: [%1$s](http://dx.doi.org/%1$s)", doi_string)
+      
+    } else {
+      doi_string <- ""
+    }
+    
+    ## impact string
+    if (!is.na(item$IF)) {
+      impact_if <- paste("IF", item$IF)
+    } else {
+        impact_if <- NULL
+        }
+    impact_r <- impact_q <- NULL
+    if (!is.na(item$rank)) impact_r <- item$rank
+    if (!is.na(item$rank)) impact_q <- item$quartile
+    impact_string <- paste0(". [",
+                            paste(c(impact_if, impact_q, impact_r),
+                                  collapse = "; "),
+                            "]")
+    
+    cat("* ")
+    cat(paste(authors_string, collapse = ", "))
+    cat(year_string, " ", sep = "")
+    cat(title_string, " ", sep = "")
+    cat(journal_string)
+    cat(vp_string)
+    cat(doi_string)
+    if (impact_string != ". []") cat(impact_string)
+    cat("\n\n")
+  }
+
+
 print_student <-
   function(item, id) {
     ## reformat promoters string
